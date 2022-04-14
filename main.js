b50ca10a7fb3823e2dcfcf087afd78de2b9ab781
intern_sb <intern_sb@localhost.localdomain>
2022-04-14 18:13:01 +0530

    day17

---
+++ main.js
@@ -0,0 +1,8 @@
+function hello() {
+console.log('hello');
+var name =
+document.getElementById("name").value;
+var password =
+document.getElementById("password").value;
+console.log("Name", name, "Password", password);
+}
